599c8a3d1e5ea548120fde96c9d31782febea6cf
David <josedavidcortesfajardo@gmail.com>
2025-08-27 20:00:15 -0500

    cambios 27-08-2025

--- database.sql
+++ database.sql
@@ -15,3 +15,34 @@ CREATE TABLE IF NOT EXISTS usuarios(
 )ENGINE=InnoDB;
 
 INSERT INTO usuarios VALUES(NULL,'admin','admin','admin@admin.com','1234','admin','imagen');
+
+CREATE TABLE IF NOT EXISTS categorias(
+    id_categoria int(11) auto_increment not null,
+    nombre_categoria varchar(100) not null,
+    CONSTRAINT pk_categoria PRIMARY KEY(id_categoria)
+)ENGINE=InnoDB;
+
+INSERT INTO categorias VALUES(NULL,'Construcción'),(NULL,'Electricos'),(NULL,'Pisos');
+
+CREATE TABLE IF NOT EXISTS productos(
+    id_producto int(11) auto_increment not null,
+    nombre_producto varchar(100) not null,
+    precio float(100,2) not null,
+    stock int(11) not null,
+    oferta varchar(50) not null,
+    fecha date not null,
+    imagen varchar(255) not null,
+    id_categoria int(11) not null,
+    CONSTRAINT pk_producto PRIMARY KEY(id_producto),
+    CONSTRAINT fk_categoria FOREIGN KEY(id_categoria) REFERENCES categorias(id_categoria)
+)ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS pedidos(
+    id_pedido int(11) auto_increment not null,
+    id_usuario int(11) not null,
+    fecha date not null,
+    total float(100,2) not null,
+    estado varchar(50) not null,
+    CONSTRAINT pk_pedido PRIMARY KEY(id_pedido),
+    CONSTRAINT fk_usuario FOREIGN KEY(id_usuario) REFERENCES usuarios(id_usuario)
+)ENGINE=InnoDB;
